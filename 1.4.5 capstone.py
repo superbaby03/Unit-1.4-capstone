779f0b5c3554f04fb5c6eb0d683fcf477253018d
smrzena630 <48065829+smrzena630@users.noreply.github.com>
2019-02-27 11:51:29 -0600

    Update 1.4.5 capstone.py

--- 1.4.5 capstone.py
+++ 1.4.5 capstone.py
@@ -1,1 +1,15 @@
-def C_change
+import PIL
+from PIL import ImageColor
+from PIL import Image
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
+import os.path
+image = Image.open('cancer.jpg')
+
+fig, axes = plt.subplots(1, 1)
+axes.imshow(image, interpolation='none')
+fig.show()
+
+image = Image.open('cancer.jpg')
+greyscale_image = image.convert('L')
+greyscale_image.save('greyscale_image.jpg')
