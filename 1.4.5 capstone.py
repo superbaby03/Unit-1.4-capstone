af710f73c433d5b3510acd7b4d7fd212b2ccb355
superbaby03 <47693832+superbaby03@users.noreply.github.com>
2019-02-28 12:09:54 -0600

    Update 1.4.5 capstone.py

--- 1.4.5 capstone.py
+++ 1.4.5 capstone.py
@@ -1,15 +1,34 @@
-import PIL
-from PIL import ImageColor
-from PIL import Image
-from PIL import ImageDraw
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt 
 import os.path
-image = Image.open('cancer.jpg')
+import numpy as np      # “as” lets us use standard abbreviations
 
-fig, axes = plt.subplots(1, 1)
-axes.imshow(image, interpolation='none')
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__)) 
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory, 'woman.jpg')
+# Read the image data into an array
+img = plt.imread('woman.jpg')
+
+'''Show the image data'''
+# Create figure with 1 subplot
+fig, ax = plt.subplots(1, 1)
+# Show the image data in a subplot
+ax.imshow(img, interpolation='none')
+# Show the figure on the screen
+
+height = len(img)
+width = len(img[0])
+for r in range(155):
+    for c in range(width):
+        if sum(img[r][c])>500: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[255,0,255] # R + B = magenta
+            
+for r in range(415,475):
+    for c in range(135,160):
+        if sum(img[r][c])>500:
+            img[r][c]=[0,255,0]
+
+
 fig.show()
-
-image = Image.open('cancer.jpg')
-greyscale_image = image.convert('L')
-greyscale_image.save('greyscale_image.jpg')
