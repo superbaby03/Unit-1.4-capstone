1a5e81dd0c7b726f6c6a1aa047bc0326b136d235
superbaby03 <47693832+superbaby03@users.noreply.github.com>
2019-03-01 11:46:47 -0600

    Update 1.4.5 capstone.py

--- 1.4.5 capstone.py
+++ 1.4.5 capstone.py
@@ -1,34 +1,41 @@
-# -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt 
+import PIL
+from PIL import ImageColor
+from PIL import Image
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
 import os.path
-import numpy as np      # “as” lets us use standard abbreviations
-
-'''Read the image data'''
-# Get the directory of this python script
+image = Image.open('cancer.jpg')
 directory = os.path.dirname(os.path.abspath(__file__)) 
-# Build an absolute filename from directory + filename
-filename = os.path.join(directory, 'woman.jpg')
-# Read the image data into an array
-img = plt.imread('woman.jpg')
+filename = os.path.join(directory, 'cancer.jpg')
+img = plt.imread('cancer.jpg')
 
-'''Show the image data'''
-# Create figure with 1 subplot
-fig, ax = plt.subplots(1, 1)
-# Show the image data in a subplot
-ax.imshow(img, interpolation='none')
-# Show the figure on the screen
+fig, axes = plt.subplots(1, 1)
+axes.imshow(img, interpolation='none')
 
 height = len(img)
 width = len(img[0])
-for r in range(155):
-    for c in range(width):
-        if sum(img[r][c])>500: # brightness R+G+B goes up to 3*255=765
-            img[r][c]=[255,0,255] # R + B = magenta
+for r in range(height):
+    for c in range(175):
+        if sum(img[r][c])<600: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[255,0,0]   
+
+height = len(img)
+width = len(img[0])
+for r in range(height):
+    for c in range(175):
+        if sum(img[r][c])<500: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[0,0,255]   
+height = len(img) 
+width = len(img[0])
+for r in range(height):
+    for c in range(175):
+        if sum(img[r][c])<100: # brightness R+G+B goes up to 3*255=765
+            img[r][c]=[0,255,255] # R + B = magenta
             
-for r in range(415,475):
-    for c in range(135,160):
-        if sum(img[r][c])>500:
-            img[r][c]=[0,255,0]
 
+            
 
+            
+            
 fig.show()
+
